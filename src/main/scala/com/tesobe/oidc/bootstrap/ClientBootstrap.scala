fd8aebcbeaf558d6800b45e859b7399127be4e56
simonredfern <simon@tesobe.com>
2025-11-26 22:17:51 +0100

    fix other-app-1

--- src/main/scala/com/tesobe/oidc/bootstrap/ClientBootstrap.scala
+++ src/main/scala/com/tesobe/oidc/bootstrap/ClientBootstrap.scala
@@ -86,19 +86,19 @@ class ClientBootstrap(authService: DatabaseAuthService, config: OidcConfig) {
       redirect_uris = "http://localhost:4242/callback"
     ),
     ClientDefinition(
-      name = "other_app_1",
+      name = "other-app-1",
       redirect_uris = "http://localhost:5175/login/obp/callback"
     ),
     ClientDefinition(
-      name = "other_app_2",
+      name = "other-app-2",
       redirect_uris = "http://localhost:5176/login/obp/callback"
     ),
     ClientDefinition(
-      name = "other_app_3",
+      name = "other-app-3",
       redirect_uris = "http://localhost:5177/login/obp/callback"
     ),
     ClientDefinition(
-      name = "other_app_4",
+      name = "other-app-4",
       redirect_uris = "http://localhost:5178/login/obp/callback"
     )
   )
